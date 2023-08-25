d6017315f5e9e978bfecc47d6186377c8097f5ee
ngquoc03 <nguyenquoc29112k3.dh@gmail.com>
2023-08-25 14:04:49 +0700

    a2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 app.get('/',(req,res) => {
     var a =1;
     var b = 2;
-    var c = b + a;
+    var c = b + a +8;
     return res.send('Hello!');
 })
 app.listen(port,() => console.log(`Example app listening at http://localhost:${port}`));
